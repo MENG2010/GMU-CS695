43cdeda7cc0ef7aa8a296ada49bdd798a9ec4676
MENG <y.meng201011@gmail.com>
2023-04-24 01:30:03 -0400

    Update data.py

--- utils/data.py
+++ utils/data.py
@@ -45,7 +45,53 @@ def load_dataset(name: str,
                          return_X_y=return_X_y,
                          return_type=return_type,
                          extract_path=downloaded_path,)
-    
+
+
+def reshape(data, extractor=None):
+    extractor = '' if not extractor else extractor.lower()
+
+    if type(data) == pd.core.frame.DataFrame:
+        data = data.to_numpy()
+    num_samples, num_values = data.shape
+    print(f'[TEST] type: {type(data)}; num_samples: {num_samples}; num_values: {num_values}')
+
+    if extractor == 'rocket':
+        num_features = 2
+    elif extractor == 'minirocket':
+        num_features = 4
+    else:
+        num_features = 1
+        
+    reshaped_data = np.zeros(shape=(num_samples, num_features, num_values//num_features))
+    print(f'[INFO] reshape {data.shape} --> {reshaped_data.shape}')
+
+    if extractor == 'rocket':
+        for sample_id in range(num_samples):
+            for val_id in range(num_values):
+                # print(f'[TEST] ({sample_id}, {val_id}): {data[sample_id][val_id]}')
+                if val_id % 2 == 0:
+                    reshaped_data[sample_id][0][val_id//2] = data[sample_id][val_id]
+                else:
+                    reshaped_data[sample_id][1][val_id//2] = data[sample_id][val_id]
+    elif extractor == 'minirocket':  # extractor is 'minirocket'
+        for sample_id in range(num_samples):
+            for val_id in range(num_values):
+                if val_id % 4 == 0:
+                    reshaped_data[sample_id][0][val_id//4] = data[sample_id][val_id]
+                elif val_id % 4 == 1:
+                    reshaped_data[sample_id][1][val_id//4] = data[sample_id][val_id]
+                elif val_id % 4 == 2:
+                    reshaped_data[sample_id][2][val_id//4] = data[sample_id][val_id]
+                else:
+                    reshaped_data[sample_id][3][val_id//4] = data[sample_id][val_id]
+    else:
+        for sample_id in range(num_samples):
+            for val_id in range(num_values):
+                reshaped_data[sample_id][0][val_id] = data[sample_id][val_id]
+
+    print(f'[INFO] After reshaped: {reshaped_data.shape}')
+    return reshaped_data
+
 
 def test():
     """Test load_dataset function."""

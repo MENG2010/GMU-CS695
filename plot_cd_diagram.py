6426591ac61fe5d5c4b467877b00e7301b23d417
MENG2010 <y.meng201011@gmail.com>
2023-05-07 19:35:13 -0400

    Update plot_cd_diagram.py

--- plot_cd_diagram.py
+++ plot_cd_diagram.py
@@ -10,9 +10,37 @@ import pandas as pd
 from plot.cd_diagram import draw_cd_diagram
 
 
-report_path = 'exp/eval'
-report_filename = 'eval_all.csv'
+report_path = 'exp/param_study/analysis'
+filenames = [
+    'summary-minirocket-svm-kernel.csv',
+    'summary-rocket-svm-kernel.csv',
+    'summary-minirocket-cnn-KernelSize.csv',
+    # 'summary-rocket-cnn-KernelSize.csv',
+    'summary-minirocket-cnn-NLayers.csv',
+    'summary-rocket-cnn-NLayers.csv',
+]
 
-report = pd.read_csv(os.path.join(report_path, report_filename), index_col=False)
-fig_filepath = 'exp/analysis/cd-diagram-eval.png'
-draw_cd_diagram(df_perf=report, title='Accuracy', labels=True, filepath=fig_filepath)
+categories = [
+    'Kernel', 'Kernel', 
+    'Kernel-Size', #'Kernel-Size',
+    'Num-Conv-Layers', 'Num-Conv-Layers',
+]
+sort_by = 'Dataset'
+metric = 'Mean-Val-Accuracy'
+
+titles = [
+    'Mean-Val-Accuracy (MiniRocket-SVM, Kernel)',
+    'Mean-Val-Accuracy (Rocket-SVM, Kernel)',
+    'Mean-Val-Accuracy (MiniRocket-CNN, Kernel-Size)',
+    # 'Mean-Val-Accuracy (Rocket-CNN, Kernel-Size)',
+    'Mean-Val-Accuracy (MiniRocket-CNN, Num-Conv-Layers)',
+    'Mean-Val-Accuracy (Rocket-CNN, Num-Conv-Layers)',
+]
+
+for report_filename, category, title in zip(filenames, categories, titles):
+    print(f'Processing {report_filename}...')
+    report = pd.read_csv(os.path.join(report_path, report_filename), index_col=False)
+    fig_filepath = os.path.join(report_path, report_filename.replace('summary', 'cd_diagram').replace('csv', 'png'))
+    draw_cd_diagram(df_perf=report, title=title, 
+                    category=category, sort_by=sort_by, metric=metric,
+                    labels=True, filepath=fig_filepath)
